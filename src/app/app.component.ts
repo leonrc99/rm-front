f9ef9d2492f13df92f34aebed5200ce4e0cb07ca
Leonardo Rodrigues <leonardo.rc999@gmail.com>
2024-11-01 19:06:08 -0300

    feat: create login page (HTML and CSS)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginPageComponent } from "./pages/login-page/login-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'rm-front';
-}
+export class AppComponent {}
